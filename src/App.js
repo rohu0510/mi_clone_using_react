2d38391e88f14b21bc62b0964ba128dbd6a5632d
rohu0510 <65508466+rohu0510@users.noreply.github.com>
2021-12-22 23:46:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import PreNavbar from './components/PreNavbar';
+import Navbar from "./components/Navbar.js"
+import { BrowserRouter as Router } from "react-router-dom"
+import Slider from "./components/Slider.js"
+import data from "./data/data.json"
+import Offers from "./components/Offers.js"
+
+function App() {
+
+return (
+<Router>
+    <PreNavbar />
+    <Navbar />
+    <Slider start={data.banner.start} />
+        <Offers offer={data.offer} />
+</Router>
+
+);
+}
+
+export default App;
